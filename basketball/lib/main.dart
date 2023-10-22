1eed7963dc0cf9e8a15bd4dd7508a756d3205b9a
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-22 19:53:15 +0300

    vertical dvider and minAxis Aligment in row widget

--- basketball/lib/main.dart
+++ basketball/lib/main.dart
@@ -16,52 +16,111 @@ class BasketBall extends StatelessWidget {
           backgroundColor: Colors.green,
           title: Text('Pointer Counter'),
         ),
-        body: Column(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              'Team A',
-              style: TextStyle(fontSize: 32),
+            Column(
+              children: [
+                Text(
+                  'Team A',
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 155),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 1 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 2 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 3 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 12,
+                )
+              ],
             ),
-            Text(
-              '0',
-              style: TextStyle(fontSize: 155),
+            VerticalDivider(
+              endIndent: 242,
+              indent: 20,
+              color: Colors.grey,
+              thickness: 1,
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, minimumSize: Size(150, 50)),
-              onPressed: () {},
-              child: Text(
-                'Add 1 Point',
-                style: TextStyle(fontSize: 19, color: Colors.black),
-              ),
+            Column(
+              children: [
+                Text(
+                  'Team B',
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 155),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 1 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 2 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green, minimumSize: Size(150, 50)),
+                  onPressed: () {},
+                  child: Text(
+                    'Add 3 Point',
+                    style: TextStyle(fontSize: 19, color: Colors.black),
+                  ),
+                ),
+                Spacer(
+                  flex: 12,
+                )
+              ],
             ),
-            const Spacer(
-              flex: 1,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, minimumSize: Size(150, 50)),
-              onPressed: () {},
-              child: Text(
-                'Add 2 Point',
-                style: TextStyle(fontSize: 19, color: Colors.black),
-              ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, minimumSize: Size(150, 50)),
-              onPressed: () {},
-              child: Text(
-                'Add 3 Point',
-                style: TextStyle(fontSize: 19, color: Colors.black),
-              ),
-            ),
-            const Spacer(
-              flex: 12,
-            )
           ],
         ),
       ),
