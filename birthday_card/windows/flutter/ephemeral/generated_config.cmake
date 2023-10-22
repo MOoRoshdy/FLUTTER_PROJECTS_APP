7aeb387d4624efaa3f1c418c7af8bd3cb3ffbc81
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-22 15:43:38 +0300

    mobile app

---
+++ birthday_card/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\FLUTTER_PROJECTS\\birthday_card" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=E:\\FLUTTER_PROJECTS\\birthday_card"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\FLUTTER_PROJECTS\\birthday_card\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\FLUTTER_PROJECTS\\birthday_card"
+  "FLUTTER_TARGET=E:\\FLUTTER_PROJECTS\\birthday_card\\lib\\circle-amge.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\FLUTTER_PROJECTS\\birthday_card\\.dart_tool\\package_config.json"
+)
