813a9e665c3de4d84d167d7c8ffa4195638771c0
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-22 21:25:48 +0300

    ui done and modify space by container with hight

--- basketball/lib/main.dart
+++ basketball/lib/main.dart
@@ -16,111 +16,118 @@ class BasketBall extends StatelessWidget {
           backgroundColor: Colors.green,
           title: Text('Pointer Counter'),
         ),
-        body: Row(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(
-                  'Team A',
-                  style: TextStyle(fontSize: 32),
-                ),
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 155),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 1 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
+                Container(
+                  height: 500,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Team A',
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(fontSize: 155),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 1 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 2 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 3 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Spacer(
-                  flex: 1,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 2 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
+                Container(
+                  height: 500,
+                  child: VerticalDivider(
+                    endIndent: 20,
+                    indent: 40,
+                    color: Colors.grey,
+                    thickness: 1,
                   ),
                 ),
-                Spacer(
-                  flex: 1,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 3 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
+                Container(
+                  height: 500,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Team B',
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(fontSize: 155),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 1 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 2 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green, minimumSize: Size(150, 50)),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 3 Point',
+                          style: TextStyle(fontSize: 19, color: Colors.black),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Spacer(
-                  flex: 12,
-                )
               ],
             ),
-            VerticalDivider(
-              endIndent: 242,
-              indent: 20,
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            Column(
-              children: [
-                Text(
-                  'Team B',
-                  style: TextStyle(fontSize: 32),
-                ),
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 155),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 1 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
-                  ),
-                ),
-                Spacer(
-                  flex: 1,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 2 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
-                  ),
-                ),
-                Spacer(
-                  flex: 1,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, minimumSize: Size(150, 50)),
-                  onPressed: () {},
-                  child: Text(
-                    'Add 3 Point',
-                    style: TextStyle(fontSize: 19, color: Colors.black),
-                  ),
-                ),
-                Spacer(
-                  flex: 12,
-                )
-              ],
-            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.green, minimumSize: Size(150, 50)),
+              onPressed: () {},
+              child: Text(
+                'Add 3 Point',
+                style: TextStyle(fontSize: 19, color: Colors.black),
+              ),
+            )
           ],
         ),
       ),
