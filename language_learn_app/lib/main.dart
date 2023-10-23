6ca47296839d7638b583256312fc2d2b82b4ed3d
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-23 17:42:17 +0300

    refactory code and bulid home page in another folder and link it in main.dart >> make new widget to less code and use it

--- language_learn_app/lib/main.dart
+++ language_learn_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:language_learn_app/screens/home_page.dart';
 
 void main() {
   runApp(TokuApp());
@@ -13,80 +14,6 @@ class TokuApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Toku'),
-        backgroundColor: Colors.brown,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            height: 65,
-            color: Colors.amber,
-            child: Text(
-              'Nembers',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            height: 65,
-            color: const Color.fromARGB(255, 7, 255, 52),
-            child: Text(
-              'FamilyMwmber',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            height: 65,
-            color: const Color.fromARGB(255, 210, 7, 255),
-            child: Text(
-              'Colors',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            width: double.infinity,
-            height: 65,
-            color: const Color.fromARGB(255, 7, 218, 255),
-            child: Text(
-              'phrases',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ language_learn_app/lib/screens/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toku'),
+        backgroundColor: Colors.brown,
+      ),
+      body: Column(
+        children: [
+          category(),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 7, 255, 52),
+            child: Text(
+              'FamilyMwmber',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 210, 7, 255),
+            child: Text(
+              'Colors',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 7, 218, 255),
+            child: Text(
+              'phrases',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class category extends StatelessWidget {
+  const category({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 24),
+      alignment: Alignment.centerLeft,
+      width: double.infinity,
+      height: 65,
+      color: Colors.amber,
+      child: Text(
+        'Nembers',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
