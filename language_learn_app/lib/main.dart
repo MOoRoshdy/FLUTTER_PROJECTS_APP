f13208641473b2fc8b0200aacd3082fd05af13a2
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-23 17:27:07 +0300

    make new statelesswidget ==>home page

--- language_learn_app/lib/main.dart
+++ language_learn_app/lib/main.dart
@@ -12,71 +12,80 @@ class TokuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Toku'),
-          backgroundColor: Colors.brown,
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: Colors.amber,
-              child: Text(
-                'Nembers',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toku'),
+        backgroundColor: Colors.brown,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: Colors.amber,
+            child: Text(
+              'Nembers',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: const Color.fromARGB(255, 7, 255, 52),
-              child: Text(
-                'FamilyMwmber',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 7, 255, 52),
+            child: Text(
+              'FamilyMwmber',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: const Color.fromARGB(255, 210, 7, 255),
-              child: Text(
-                'Colors',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 210, 7, 255),
+            child: Text(
+              'Colors',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: const Color.fromARGB(255, 7, 218, 255),
-              child: Text(
-                'phrases',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            height: 65,
+            color: const Color.fromARGB(255, 7, 218, 255),
+            child: Text(
+              'phrases',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
