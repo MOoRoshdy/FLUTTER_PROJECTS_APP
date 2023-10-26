104126b84fd6fc448e1f410acc39eff38e47b353
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-26 20:56:27 +0300

    make container in numbers page contine row widget

--- language_learn_app/lib/screens/numbers_page.dart
+++ language_learn_app/lib/screens/numbers_page.dart
@@ -13,9 +13,29 @@ class NumbersPage extends StatelessWidget {
           backgroundColor: Colors.amber,
         ),
         body: Container(
+          color: const Color.fromARGB(255, 179, 138, 16),
+          height: 100,
           child: Row(
             children: [
-              
+              Image.asset('assets/images/numbers/number_one.png'),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'ichi',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Text(
+                    'one',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 32,
+              )
             ],
           ),
         ),
