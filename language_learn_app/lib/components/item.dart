65931c6e19db9d54da7da37da3c941af7c3bd215
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-26 21:57:29 +0300

    add new commbunat item and refactoring number

---
+++ language_learn_app/lib/components/item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:language_learn_app/models/number.dart';
+
+class Item extends StatelessWidget {
+  const Item({Key? key,required this.number}):super(key:key);
+  final Number number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 179, 138, 16),
+      height: 100,
+      child: Row(
+        children: [
+          Container(color: Color(0xffFFF6DC), child: Image.asset(number.image)),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  number.jpName,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  number.enName,
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
