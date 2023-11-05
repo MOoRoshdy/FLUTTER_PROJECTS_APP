d910c41dbb26a5eb299ed398de05765c0a099ee6
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-11-05 13:36:25 +0200

    use onsubmitied to store data in vairable

--- weather_app/lib/pages/search_page.dart
+++ weather_app/lib/pages/search_page.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
+  String? City_Name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Search a City')),
       body: Center(
-        child: TextField(
-          decoration: InputDecoration(
-              label: Text('Search'),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
-              prefixIcon: Icon(Icons.search),
-              hintText: 'City Name'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: TextField(
+            onSubmitted: (data) {
+              City_Name = data;
+            },
+            decoration: InputDecoration(
+                label: Text('Search'),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
+                prefixIcon: Icon(Icons.search),
+                hintText: 'City Name'),
+          ),
         ),
       ),
     );
