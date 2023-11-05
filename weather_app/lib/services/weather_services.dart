50d5e1bb37560a140b152b00c6f697c2414f91f0
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-11-05 16:59:48 +0200

    formating the api to make it esy to use

--- weather_app/lib/services/weather_services.dart
+++ weather_app/lib/services/weather_services.dart
@@ -1,3 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class WeatherServices {
-  void get_wheather({required String CityName}) {}
+  String BaseUrl = 'http://api.weatherapi.com/v1';
+  String ApiKey = '91735585648845c08c2201533230311';
+  void get_wheather({required String CityName}) async {
+    Uri url = Uri.parse(
+        '$BaseUrl/forecast.json?key=$ApiKey&q=$CityName&days=1');
+    http.Response response = await http.get(url);
+    Map<String, dynamic> data = jsonDecode(response.body);
+  }
 }
